2165b3445227fecc81144bf51f132bc84925739c
shibo-wang <wangshibo19861010@163.com>
2017-04-02 16:22:40 +0800

    [EHN] refator the GPIO config.

---
+++ Projects/Code_Jbike_Motor_TEST_V1.0_170331/jb_config.h
@@ -0,0 +1,35 @@
+// File Name   		:  jb_config.H
+#ifndef		__INIT_H__
+#define		__INIT_H__
+
+// U,V,W PWM output
+#define GPIO_PIN_PWM_U_P 	GPIO_Pin_8
+#define GPIO_PIN_PWM_V_P 	GPIO_Pin_9
+#define GPIO_PIN_PWM_W_P 	GPIO_Pin_10
+#define GPIO_PORT_PWM_UVW_P	GPIOA
+
+#define GPIO_PIN_PWM_U_N 	GPIO_Pin_13
+#define GPIO_PIN_PWM_V_N 	GPIO_Pin_14
+#define GPIO_PIN_PWM_W_N 	GPIO_Pin_15
+#define GPIO_PORT_PWM_UVW_N GPIOB
+
+// LED output
+#define GPIO_PIN_LED_FLASH 	GPIO_Pin_2
+#define GPIO_PORT_LED_FLASH GPIOB
+
+
+// UVW HALL in
+#define GPIO_PIN_HALL_U 	GPIO_Pin_1
+#define GPIO_PIN_HALL_V 	GPIO_Pin_11
+#define GPIO_PIN_HALL_W 	GPIO_Pin_15
+#define GPIO_PORT_HALL_UVW 	GPIOA
+
+
+
+
+
+
+
+
+#endif
+
