d9268fe62f2aa1766b2dd031bd08ff6331333b52
linjunrong <946391381@qq.com>
2017-04-02 15:06:41 +0800

    Revert "delete BSP"
    This reverts commit fe113340f3ad7c73373550e11de914c5dab3cd64.

---
+++ BSP/LED.h
@@ -0,0 +1,19 @@
+//=============================================================================
+//文件名称：LED.h
+//功能概要：LED驱动头文件
+//版权所有：源地工作室www.vcc-gnd.com
+//淘宝网店：http://vcc-gnd.taobao.com
+//更新时间：2013-12-31
+//=============================================================================
+#ifndef __LED_H
+#define __LED_H
+
+#include "stm32f0xx.h"
+
+#define LED_OFF                       GPIOB->BSRR = 0x02
+#define LED_ON                        GPIOB->BRR = 0x02 
+#define LED_TURN                      GPIOB->ODR ^= 0x02
+
+void LED_Init(void);
+
+#endif
